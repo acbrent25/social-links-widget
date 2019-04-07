058a1a24b63e80511e71a8a808cb51a85ef5721b
acbrent25 <adamchampagne@gmail.com>
2019-04-07 06:21:39 -0400

    plugin setup

---
+++ social-links.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name: Socail Links 
+Description: Ads social icons with links to profiles
+Version: 1.0
+Author: Adam Champagne
+Auther URI: https://adamchampagne.com/
+*/
+
+ // Exit if Accessed Directly
+ if(!defined('ABSPATH')){
+     exit;
+ }
