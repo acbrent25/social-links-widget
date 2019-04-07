61ddb3f6132dff53035c3229d2033e96f06364e8
acbrent25 <adamchampagne@gmail.com>
2019-04-07 06:39:59 -0400

    add widget class

---
+++ includes/social-links-class.php
@@ -0,0 +1,52 @@
+<?php
+
+class Social_Links_Widget extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'Social_Links_Widget',
+			'description' => 'Social Links to Icon',
+		);
+		parent::__construct( 'Social_Links_Widget', 'Social Links Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+        // outputs the content of the widget
+        ?>
+            TEST FRONTEND
+        <?php
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+        // outputs the options form on admin
+        ?>
+            TEST BACKEND FORM
+        <?php
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 *
+	 * @return array
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
